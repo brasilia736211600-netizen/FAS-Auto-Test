fefccfadcd71eb19da5e5f5653a42b89ddd7b16c
brasilia736211600-netizen <brasilia736211600@gmail.com>
2026-09-13 22:59:42 +0300

    fix: install complete FAS runtime modules

--- install_fas.sh
+++ install_fas.sh
@@ -8,7 +8,8 @@ BIN_DIR="${FAS_BIN_DIR:-$HOME/.local/bin}"
 mkdir -p "$FAS_HOME" "$BIN_DIR"
 cp "$SOURCE_DIR/model_router.py" "$SOURCE_DIR/fas_runtime.py" "$SOURCE_DIR/fas_fallback.py" \
    "$SOURCE_DIR/fas_git.py" "$SOURCE_DIR/fas_ci.py" "$SOURCE_DIR/fas_github.py" \
-   "$SOURCE_DIR/fas_orchestrator.py" "$SOURCE_DIR/fas_cli.py" "$FAS_HOME/"
+   "$SOURCE_DIR/fas_orchestrator.py" "$SOURCE_DIR/fas_recovery.py" \
+   "$SOURCE_DIR/fas_watch.py" "$SOURCE_DIR/fas_cli.py" "$FAS_HOME/"
 
 cat > "$BIN_DIR/fas" <<'EOF'
 #!/usr/bin/env bash
